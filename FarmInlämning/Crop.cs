f0363b1d12ad4dc17f6a8d38e7dbeb6d468635ae
Rodi Dogan <Arjandogan@gmail.com>
2023-10-22 13:17:23 +0200

    Update Crop.cs

--- FarmInlämning/Crop.cs
+++ FarmInlämning/Crop.cs
@@ -1,12 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Dynamic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace FarmInlämning
 {
-    internal class Crop
+    internal class Crop : Entity
     {
+
+        List <Crop> crop = new List<Crop>();
+
+        public Crop()
+        {
+            Crop crop = new Crop("Häst",)
+        }
+
+        }
+        public string cropType;
+        private int quantity;
+
+        public override void GetDescription()
+        {
+            string CropInfo ("The type of crop is: " + cropType + " and the Quantity of them are: " + quantity );
+            Console.WriteLine(CropInfo);
+        }
     }
 }
